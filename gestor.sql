a2625916b5a86edb96afc5b8742ae3e4e8d91b69
luisbarroncortez <77166748+luisbarroncortez@users.noreply.github.com>
2021-01-08 11:36:31 -0600

    Add files via upload

---
+++ gestor.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-01-2021 a las 06:08:37
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gestor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_archivos`
+--
+
+CREATE TABLE `t_archivos` (
+  `id_archivo` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `tipo` varchar(255) DEFAULT NULL,
+  `ruta` text DEFAULT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_categorias`
+--
+
+CREATE TABLE `t_categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `fechaInsert` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_usuarios`
+--
+
+CREATE TABLE `t_usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `fechaNacimiento` date DEFAULT NULL,
+  `email` varchar(245) DEFAULT NULL,
+  `usuario` varchar(245) DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `insert` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `t_usuarios`
+--
+
+INSERT INTO `t_usuarios` (`id_usuario`, `nombre`, `fechaNacimiento`, `email`, `usuario`, `password`, `insert`) VALUES
+(11, 'Luis Barron Cortez', '2020-12-11', 'luisbarroncortez@gmail.com', 'Luis Barron Cortez', '6a62415b43a0268f38f117f076dce135705d7895', '2020-12-29 12:56:06');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `t_archivos`
+--
+ALTER TABLE `t_archivos`
+  ADD PRIMARY KEY (`id_archivo`),
+  ADD KEY `fkArchivosCategorias_idx` (`id_categoria`),
+  ADD KEY `fkUsuariosArchivos_idx` (`id_usuario`);
+
+--
+-- Indices de la tabla `t_categorias`
+--
+ALTER TABLE `t_categorias`
+  ADD PRIMARY KEY (`id_categoria`),
+  ADD KEY `fkCategoriaUsuario_idx` (`id_usuario`);
+
+--
+-- Indices de la tabla `t_usuarios`
+--
+ALTER TABLE `t_usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `t_archivos`
+--
+ALTER TABLE `t_archivos`
+  MODIFY `id_archivo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `t_categorias`
+--
+ALTER TABLE `t_categorias`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `t_usuarios`
+--
+ALTER TABLE `t_usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `t_archivos`
+--
+ALTER TABLE `t_archivos`
+  ADD CONSTRAINT `fkArchivosCategorias` FOREIGN KEY (`id_categoria`) REFERENCES `t_categorias` (`id_categoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fkUsuariosArchivos` FOREIGN KEY (`id_usuario`) REFERENCES `t_usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `t_categorias`
+--
+ALTER TABLE `t_categorias`
+  ADD CONSTRAINT `fkCategoriaUsuario` FOREIGN KEY (`id_usuario`) REFERENCES `t_usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
